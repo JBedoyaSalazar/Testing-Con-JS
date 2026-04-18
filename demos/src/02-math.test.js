323cdc6de245be49a09030a0438597fa6492e2f1
Jefred Bedoya <jefred03salazar@gmail.com>
2026-04-18 11:54:14 -0500

    test: add unit tests for Person class and its IMC calculation

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,33 +1,52 @@
 const { sum, multiply, divide, calculateTotalToPay } = require('./02-math');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+describe('Test for math', () => {
 
-test('multiplying 2 * 2 to equal 4', () => {
-  expect(multiply(2, 2)).toBe(4);
-});
+  describe('sum', () => {
+    test('adds 1 + 2 to equal 3', () => {
+      expect(sum(1, 2)).toBe(3);
+    });
+  })
 
-test('should divide', () => {
-  expect(divide(6, 2)).toBe(3);
+  describe('multiply', () => {
+    test('multiplying 2 * 2 to equal 4', () => {
+      expect(multiply(2, 2)).toBe(4);
+    });
+  })
 
-  expect(divide(10, 2)).toBe(5);
+  describe('divide', () => {
+    test('should divide', () => {
+      expect(divide(6, 2)).toBe(3);
 
-  expect(divide(5, 2)).toBe(2.5);
-});
+      expect(divide(10, 2)).toBe(5);
 
-test('should divide for 0', () => {
-  expect(divide(6, 0)).toBe(null);
+      expect(divide(5, 2)).toBe(2.5);
+    });
 
-  expect(divide(10, 0)).toBe(null);
+    test('should divide for 0', () => {
+      expect(divide(6, 0)).toBe(null);
 
-  expect(divide(5, 2)).toBe(2.5);
-});
+      expect(divide(10, 0)).toBe(null);
 
-test('Calculate discount', () => {
-  expect(calculateTotalToPay(100, 105)).toBe(null);
+      expect(divide(5, 2)).toBe(2.5);
+    });
 
-  expect(calculateTotalToPay(100, 90)).toBe(10);
+  })
 
-  expect(calculateTotalToPay(100, 100)).toBe(0);
-});
+  describe('calculte total to pay', () => {
+    test('Calculate total to pay', () => {
+      expect(calculateTotalToPay(100, 105)).toBe(null);
+
+      expect(calculateTotalToPay(100, 90)).toBe(10);
+
+      expect(calculateTotalToPay(100, 100)).toBe(0);
+    });
+  })
+})
+
+
+
+
+
+
+
