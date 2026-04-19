ce5ebd44d14820eb48c76931e4ef670416f91743
Jefred Bedoya <jefred03salazar@gmail.com>
2026-04-19 02:34:30 -0500

    fix: update e2e test configuration file name in package.json

--- api/e2e/hello.e2e.js
+++ api/e2e/hello.e2e.js
@@ -2,8 +2,26 @@ const request = require('supertest');
 
 const createApp = require('../src/app');
 
-describe('Test for Hello end point', () => {
+describe('Hello Endpoint', () => {
+  let app;
+  let server;
+
   beforeAll(() => {
     app = createApp();
-  })
+    server = app.listen(3001);
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  describe('GET /', () => {
+    test('should return 200 status with "Hello World!" message', async () => {
+      const response = await request(app)
+        .get('/')
+        .expect(200);
+
+      expect(response.text).toBe('Hello World!');
+    });
+  });
 });
